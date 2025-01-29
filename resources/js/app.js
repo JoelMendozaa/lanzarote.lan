1e80d479b9f848f71570e1f807c9d9b8204e621d
Joel <yowismr@gmail.com>
2025-01-29 12:50:57 +0000

    Instalar Breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap';
+import './bootstrap';
 
-import '../css/estilos.css';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
