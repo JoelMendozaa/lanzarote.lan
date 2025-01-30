e2f41ddbfaaefd1ffb134165cf26a0f333592a13
Joel <yowismr@gmail.com>
2025-01-30 09:13:26 +0000

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,10 @@
-import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap';
+
+
+import '../css/app.css';
+import '../css/estilos.css';
 
 import Alpine from 'alpinejs';
 
